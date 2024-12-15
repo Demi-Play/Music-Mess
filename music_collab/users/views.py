62b73d0604861e1100c21d3f3cd06adf45229cf8
D3M1.dev <63244338+Demi-Play@users.noreply.github.com>
2024-12-15 10:32:04 +0300

    small fix

--- music_collab/users/views.py
+++ music_collab/users/views.py
@@ -19,11 +19,14 @@ def login_view(request):
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(request, username=username, password=password)
+        
         if user is not None:
             login(request, user)
             return redirect('projects:project_list')  # Перенаправление на список проектов
         else:
+            # Передаем сообщение об ошибке в контекст
             return render(request, 'users/login.html', {'error': 'Неверные учетные данные'})
+    
     return render(request, 'users/login.html')
 
 def logout_view(request):
